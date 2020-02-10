c787e0834c5de867f08b5b8b669263d00da82876
Heli Sivunen <heli@mooncat.tv>
2020-02-10 13:34:26 +0000

    Create bare bones, still working on conversion of string to integer.

---
+++ Happy-index-VisualStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Happy_index_VisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
